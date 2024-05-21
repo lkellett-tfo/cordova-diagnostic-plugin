16a07db60ab787ec31812a553fd675f7542c6b71
Luke Kellett <luke@thefleetoffice.com.au>
2024-05-21 11:19:04 +1000

    added switchToNotificationSettings function for iOS platform

--- www/ios/diagnostic.notifications.js
+++ www/ios/diagnostic.notifications.js
@@ -183,6 +183,17 @@ var Diagnostic_Notifications = (function(){
             [params.types, params.omitRegistration ? 1 : 0]);
     };
 
+    /**
+     * Switches to the notifications page in the Settings app
+     */
+    Diagnostic_Notifications.switchToNotificationSettings = function() {
+        return cordova.exec(null,
+            null,
+            'Diagnostic_Notifications',
+            'switchToNotificationSettings',
+            []);
+    };
+
 
     return Diagnostic_Notifications;
 });
